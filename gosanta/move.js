aab26eb0782468f4418c133ed6829640b08229a5
Artin A <iamdevandrei@gmail.com>
2018-01-04 11:32:00 +0200

    code imrpovement

--- gosanta/move.js
+++ gosanta/move.js
@@ -3,7 +3,7 @@ var stopped = false;
 var santa = document.getElementById('santa');
 var santaTop = 0;
 var santaLeft = 0;
-var stepNr = 0;
+var stepNr = 0; //for animation 
 var lastPosition ='';
 
 function stop(e){
@@ -11,19 +11,18 @@ function stop(e){
     console.log(document.body);
     stopped = true;
     stepNr = 0;
-    santa.className='';
+    santa.className=''; 
     if(lastPosition =='right'){
         santa.className='santa'; 
     }else if(lastPosition =='left'){
         santa.className='santa stopLeft'; 
-    }
-    
-   
+    } 
 }
 
 function move(e){
     document.body.removeAttribute('onkeydown','move(event)');
     stopped = false;
+    santa.className ='santa';
     santaTop = santa.offsetTop;
     santaLeft = santa.offsetLeft;
     if(e.code == 'ArrowRight'){
@@ -34,9 +33,9 @@ function move(e){
         startMove('left');
        
     }else if(e.code == 'ArrowUp'){
-        starMove('up');
+        startMove('up');
     }else if(e.code == 'ArrowDown'){
-        starMove('down');
+        startMove('down');
     }
     
     
@@ -45,27 +44,15 @@ function move(e){
 function startMove(nav){
     if(nav == 'right'){     
         setTimeout(function(){
-            if(stepNr > 9){
-                santa.classList.remove(`r_${stepNr}`);
-                stepNr=0;
-            }
                 if(stopped != true){
-                    if(santaLeft>=745){
-                        santaLeft=0;
-                    }
+                    santaLeft>=745 ? santaLeft=0 : santaLeft +=5;
                     santa.classList.remove(`r_${stepNr}`);
-                    stepNr++;
-                    santaLeft +=5;
+                    stepNr > 9 ? stepNr=1 : stepNr++;   
                     santa.classList.add(`r_${stepNr}`);
                     santa.style.marginLeft = `${santaLeft}px`;
-
-                    console.log("alo",santa);
                     startMove(nav);
-                
                 }
-           
-            
-        },100);
+        },80);
     }else if(nav == 'left'){
         setTimeout(function(){
             if(stepNr > 9){
@@ -73,9 +60,7 @@ function startMove(nav){
                 stepNr=0;
             }
             if(stopped != true){
-                if(santaLeft<=0){
-                    santaLeft=745;
-                }
+                santaLeft<=0 ? santaLeft=745 : "";
                 santa.classList.remove(`l_${stepNr}`);
                 stepNr++;
                 santaLeft -=5;
@@ -84,7 +69,9 @@ function startMove(nav){
                 console.log(santaLeft);
                 startMove(nav);
             }
-        },100);
+        },80);
+    }else if(nav == 'up'){
+        console.log('up');
     }
     
     console.log(santa);
